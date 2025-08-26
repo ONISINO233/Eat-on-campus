1ede05eba06ff9c1c95d183e40eb032d106b1d52
ONISINO <onisino@outlook.com>
2025-08-26 23:04:31 +0800

    项目初始化

---
+++ src/main/java/onisino/eatoncampus/EatOnCampusApplication.java
@@ -0,0 +1,13 @@
+package onisino.eatoncampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatOnCampusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatOnCampusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/onisino/eatoncampus/EatOnCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package onisino.eatoncampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatOnCampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
